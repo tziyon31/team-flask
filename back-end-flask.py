ebc72e7286907011bad55cf7938f647190461460
Tziyon Bublil <tziyon-bublil@tziyon-bublil.com>
2025-11-19 13:04:25 +0200

    adding post route

--- back-end-flask.py
+++ back-end-flask.py
@@ -1,4 +1,6 @@
 from flask import Flask, request, render_template
+from datetime import datetime
+import os
 
 app = Flask(__name__)
 
@@ -8,13 +10,20 @@ def home():
     return render_template("index.html")
 
 
-    @app.route('/<room>')
-    def room(room):
-        return render_template('index.html', room=room)
+@app.route('/<room>')
+def room(room):
+    return render_template('index.html', room=room)
 
-    @app.route('/api/chat/<room>', methods=['POST'])
-    def chat(room):
-        return "Hello, World!"
-    if __name__ == '__main__':
-          app.run(debug=True)
+@app.route('/api/chat/<room>', methods=['POST'])
+def chat(room):
+    username = request.form.get('username')
+    msg = request.form.get('msg')
+    timestamp = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    message = f"{timestamp}  {username}: {msg} "
+    filename = f"chat_{room}.txt"
+    with open(filename, "a", encoding='utf-8') as f:
+        f.write(message + "\n")
+    return "", 204
+if __name__ == '__main__':
+    app.run(debug=True)
 
