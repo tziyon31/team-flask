1c91ce859dcc7e927d86153a98ef02f1ed3c6dd3
Tziyon Bublil <tziyon-bublil@tziyon-bublil.com>
2025-11-19 11:44:09 +0200

    room path commit

---
+++ back-end-flask.py
@@ -0,0 +1,17 @@
+from flask import Flask, request, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return "Hello, World!"
+
+if __name__ == '__main__':
+    app.run(debug=True)
+    
+    @app.route('/<room>')
+    def room(room):
+        return render_template('index.html', room=room)
+    @app.route('/api/chat/<room>', methods=['POST'])
+    def chat(room):
+        return "Hello, World!"
