e6f0cc002de5c2de41400c0d899efc1ea35f2f33
Tziyon Bublil <tziyon-bublil@tziyon-bublil.com>
2025-11-19 12:07:53 +0200

    small commit

--- back-end-flask.py
+++ back-end-flask.py
@@ -1,17 +1,15 @@
 from flask import Flask, request, render_template
 
 app = Flask(__name__)
+    
+@app.route('/<room>')
+def room(room):
+        return render_template('index.html', room=room)
+@app.route('/api/chat/<room>', methods=['POST'])
+def chat(room):
+        
 
-@app.route('/')
-def index():
-    return "Hello, World!"
+
 
 if __name__ == '__main__':
     app.run(debug=True)
-    
-    @app.route('/<room>')
-    def room(room):
-        return render_template('index.html', room=room)
-    @app.route('/api/chat/<room>', methods=['POST'])
-    def chat(room):
-        return "Hello, World!"
